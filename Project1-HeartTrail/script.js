df50b8d1f709cb6ef9e8fd9d8b001c3b813c9858
rooshan008 <callmegod7550@gmail.com>
2025-03-18 12:02:31 +0530

    formate

--- Project1-HeartTrail/script.js
+++ Project1-HeartTrail/script.js
@@ -1,24 +1,19 @@
-const bodyEl = document.querySelector("body")
+const bodyEl = document.querySelector('body')
 
+bodyEl.addEventListener('mousemove', (e) => {
+  const xPos = e.offsetX
+  const yPos = e.offsetY
 
+  const spanEl = document.createElement('span')
+  bodyEl.appendChild(spanEl)
 
+  spanEl.style.left = xPos + 'px'
+  spanEl.style.top = yPos + 'px'
+  const size = Math.floor(Math.random() * 100)
+  spanEl.style.width = size + 'px'
+  spanEl.style.height = size + 'px'
 
-
-bodyEl.addEventListener("mousemove", (e) => {
-    const xPos = e.offsetX
-    const yPos = e.offsetY
-
-    const spanEl = document.createElement('span')
-    bodyEl.appendChild(spanEl)
-
-    spanEl.style.left = xPos + 'px'
-    spanEl.style.top = yPos + 'px'
-    const size = Math.floor(Math.random() *100)
-    spanEl.style.width = size + 'px'
-    spanEl.style.height = size + 'px'
-
-    setTimeout(() => {
-        spanEl.remove()
-    }, 3000);
-
+  setTimeout(() => {
+    spanEl.remove()
+  }, 3000)
 })
